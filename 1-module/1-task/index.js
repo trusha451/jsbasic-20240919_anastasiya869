b377b9ca36e172ec5154e5c328a8f3db4ac88dee
Anastasia <trusha451@gmail.com>
2024-09-19 06:50:30 +0300

    Тесты пройдены

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,10 +1,11 @@
 function factorial(n) {
-  let result = 1
   if(n==0)
-    return result
+    return 1
   else{
+    let result = n
     for(let i = n-1; i>0; i--){
       result *= i
     }
+    return result
   }
 }
