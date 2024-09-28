8ea5f1b8c8455435d9cb443236fc403640e26da9
Трушкина Анастасия Александровна <Trushkina.Anastasiya@luis.ru>
2024-09-28 20:48:40 +0300

    2-module решен

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,5 +1,20 @@
 let calculator = {
-  // ваш код
+  a: 0,
+  b: 0,
+
+  read: function (a, b) {
+    this.a = a
+    this.b = b
+  },
+
+  sum: function() {
+    return this.a + this.b
+  },
+
+  mul: function(){
+    return this.a * this.b
+  }
+
 };
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
