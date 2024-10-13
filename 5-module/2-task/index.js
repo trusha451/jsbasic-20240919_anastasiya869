642f424a22d644a10f732014bc5ed39b3e6d28f5
Трушкина Анастасия Александровна <Trushkina.Anastasiya@luis.ru>
2024-10-13 18:48:51 +0300

    5-module. 1-task, 2-task

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,11 @@
 function toggleText() {
-  // ваш код...
+  let toggleButton = document.querySelector('.toggle-text-button')
+  toggleButton.onclick = () => {
+    let objText = document.getElementById('text')
+    if (objText.hidden == true){
+      objText.hidden = false
+    }else{
+      objText.hidden = true
+    }
+  }
 }
